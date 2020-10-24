cb16bc43618e28740533782917fffb456530d214
Mayank Singh <68012074+fms12@users.noreply.github.com>
2020-10-24 16:51:49 +0530

    Add files via upload

---
+++ quicksort.py
@@ -0,0 +1,21 @@
+def partition(arr, low, high):
+    i = low-1
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return (i + 1)
+
+
+def quick_sort(arr, low, high):
+    if low < high:
+        p = partition(arr, low, high)
+        quick_sort(arr, low, p - 1)
+        quick_sort(arr, p + 1, high)
+
+arr=[9,8,7,6,5,4,4,3,1]
+quick_sort(arr, 0, len(arr) - 1)
+print(arr)
+
